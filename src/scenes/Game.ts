59e1a9fe836826073ac30d98d7c1a5684b59621a
John Ketiku <144270920+John-Ketiku-1@users.noreply.github.com>
2024-12-16 15:42:34 +0000

    hello world ez

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -19,8 +19,8 @@ export class Game extends Scene
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0.5);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        this.msg_text = this.add.text(512, 384, 'Hello,World!', {
+            fontFamily: 'Arial Black', fontSize: 50, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         });
