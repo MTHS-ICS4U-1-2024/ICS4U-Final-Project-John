899db283baee217fb1ed22f7e1679a1343dca622
John Ketiku <144270920+John-Ketiku-1@users.noreply.github.com>
2024-12-18 14:30:36 +0000

    added image files to asse3ts

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -17,7 +17,7 @@ export class MainMenu extends Scene
 
         this.logo = this.add.image(512, 300, 'logo');
 
-        this.title = this.add.text(512, 460, 'Click to see hellow world.', {
+        this.title = this.add.text(512, 460, 'Click to see Hello World.', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
